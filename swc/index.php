81427d9840eb77d8894c9331faee54bbcca6febf
code-syl <ejjvandelaar@gmail.com>
2024-04-16 15:14:32 +0200

    refactor + swc tools init

--- swc/index.php
+++ swc/index.php
@@ -1,1 +1,31 @@
-Hello New Domain!
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <title>SWC Tools by Code-Syl</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Code-Syl" />
+    <meta name="description" content="Tools for the browser game Star Wars Combine, by Code-Syl" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="/styles/common.css" type="text/css" media="screen" />
+    <script src="/components/main-content/main-content.component.js" type="module" name="main-content" defer></script>
+    <script src="/components/under-construction/under-construction.component.js" type="module" name="under-construction" defer>
+    </script>
+    <script src="/components/nav-bar/nav-bar.component.js" type="module" name="nav-bar" defer></script>
+</head>
+
+<!-- thanks to https://stackoverflow.com/a/5983338 for the FOUC test fix -->
+
+<body onload="document.body.style.visibility=`visible`;">
+    <script>
+        document.body.style.visibility = `hidden`;
+    </script>
+
+    <main-content>
+        Hello SWC Tools!
+    </main-content>
+</body>
+
+</html>
