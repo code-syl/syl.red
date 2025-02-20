d7ee577d92a33e5eaf26b27fbe0a6809c914879f
code-syl <93523097+code-syl@users.noreply.github.com>
2025-02-20 15:43:17 +0100

    more header work + icons

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="/styles/common.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="/styles/window.css" type="text/css" media="screen" />
 
     <script src="/scripts/window.js" type="text/javascript" defer></script>
 
@@ -23,7 +24,20 @@
     </script>
 
     <div class="canvas">
-        <div class="win95_window" id="main_window"></div>
+        <div class="win95_window" id="main_window">
+            <div class="win95_header" id="main_window_header">
+                <span class="win95_header_text">Welcome!</span>
+                <div class="win95_header_controls">
+                    <div class="win95_header_control">
+                        <div class="win95_header_control_question"></div>
+                    </div>
+                    <div class="win95_header_control">
+                        <div class="win95_header_control_cross"></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
 
 </body>
