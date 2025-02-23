63a7c2c0adaab8aecd6a939966e1430c0384a8fe
code-syl <93523097+code-syl@users.noreply.github.com>
2025-02-23 12:31:35 +0100

    generalize drag event

--- scripts/window.js
+++ scripts/window.js
@@ -1,23 +1,32 @@
-const _identifier = "main_window";
-let _main_window_draggable = document.getElementById(_identifier);
+const _main_window_identifier = "main_window";
+
+let _main_window = document.getElementById(_main_window_identifier);
 const _container = document.querySelector(".canvas");
 let _main_window_draggable_header = document.getElementById(
-    _identifier + "_header"
+    _main_window_identifier + "_header"
 );
-_main_window_draggable_header.addEventListener("mousedown", _drag);
-_main_window_draggable_header.addEventListener("touchstart", _drag, {
-    passive: false,
+_main_window_draggable_header.addEventListener("mousedown", (event) => {
+    _drag(event, _main_window);
 });
+_main_window_draggable_header.addEventListener(
+    "touchstart",
+    (event) => {
+        _drag(event, _main_window);
+    },
+    {
+        passive: false,
+    }
+);
 
 // derived from https://infinitejs.com/posts/keep-dragged-elements-within-parent/
-function _drag(event) {
+function _drag(event, element) {
     event.preventDefault();
 
     let clientX = event.clientX || event.touches[0].clientX;
     let clientY = event.clientY || event.touches[0].clientY;
 
-    let offsetX = clientX - _main_window_draggable.getBoundingClientRect().left;
-    let offsetY = clientY - _main_window_draggable.getBoundingClientRect().top;
+    let offsetX = clientX - _main_window.getBoundingClientRect().left;
+    let offsetY = clientY - _main_window.getBoundingClientRect().top;
 
     document.addEventListener("mousemove", _onMove);
     document.addEventListener("mouseup", _stop);
@@ -36,7 +45,7 @@ function _drag(event) {
         let newY = clientY - offsetY;
 
         const containerRectangle = _container.getBoundingClientRect();
-        const elementRectangle = _main_window_draggable.getBoundingClientRect();
+        const elementRectangle = element.getBoundingClientRect();
 
         // restrict movement within the container
         newX = Math.max(
@@ -48,9 +57,8 @@ function _drag(event) {
             Math.min(newY, containerRectangle.bottom - elementRectangle.height)
         );
 
-        _main_window_draggable.style.left =
-            newX - containerRectangle.left + "px";
-        _main_window_draggable.style.top = newY - containerRectangle.top + "px";
+        _main_window.style.left = newX - containerRectangle.left + "px";
+        _main_window.style.top = newY - containerRectangle.top + "px";
     }
 
     function _stop() {
