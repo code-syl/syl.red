7260d4ae3488632a0eb4d54ce76138128628569c
code-syl <93523097+code-syl@users.noreply.github.com>
2025-08-26 14:23:57 +0200

    setup

---
+++ nav/swc/map/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <title>SWC Map Test -- Syl.red</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Code-Syl" />
+    <meta name="description" content="A test to draw a map for SWC, by Code-Syl" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="/nav/swc/map/styles.css" type="text/css" media="screen" />
+
+    <script src="/nav/swc/vision_sim/map.js" defer></script>
+</head>
+
+<body>
+
+
+</body>
