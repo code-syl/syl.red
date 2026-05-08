5b12d80b8cc258edf5d6fdd680277c9e2fd79483
code-syl <93523097+code-syl@users.noreply.github.com>
2026-05-08 12:35:05 +0200

    subdomain test

---
+++ nav/notes/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <title>Notes</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Code-Syl" />
+    <meta name="description" content="WIP mock site for notes" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    Hi there.
+</body>
