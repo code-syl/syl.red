177ec7883ee3dda4367451dc06514483d5ad1795
code-syl <93523097+code-syl@users.noreply.github.com>
2026-05-08 16:58:04 +0200

    notes page - top banner

--- nav/notes/index.php
+++ nav/notes/index.php
@@ -7,9 +7,22 @@
     <meta charset="UTF-8">
     <meta name="author" content="Code-Syl" />
     <meta name="description" content="WIP mock site for notes" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=1100">
+
+    <link rel="stylesheet" href="./notes/styles.css" type="text/css" media="screen" />
+
 </head>
 
 <body>
-    Hi there.
+    <header class="banner">
+        <div class="banner-avatar-left">
+            <img src="/assets/logo64.png" />
+        </div>
+        <div class="banner-title-box">
+            <span class="banner-title">code-syl</span>
+            <span class="banner-subtitle">hacking &middot; learning &middot; sharing</span>
+        </div>
+    </header>
+    <main class="main-content">
+    </main>
 </body>
